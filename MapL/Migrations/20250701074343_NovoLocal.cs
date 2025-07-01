833afac76e8cf0cab225b0b9a7c8326ead18ddda
Eduardo <eduardoviniciusjorge@gmail.com>
2025-07-01 05:13:46 -0300

    novo local

---
+++ MapL/Migrations/20250701074343_NovoLocal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MapL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovoLocal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
