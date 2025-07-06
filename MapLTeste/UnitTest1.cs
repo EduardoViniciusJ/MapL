657ce7309d6717c1c31500342142607e6da3a141
Eduardo <eduardoviniciusjorge@gmail.com>
2025-07-06 18:15:45 -0300

    adicionando ambiente de testes

---
+++ MapLTeste/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace MapLTeste
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+
+
+        }
+    }
+}
--- MapLTeste/obj/Debug/net8.0/MapLTeste.AssemblyInfo.cs
+++ MapLTeste/obj/Debug/net8.0/MapLTeste.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MapL")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MapLTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d69873585c513158fdb6eb20c40144c1d66c915")]
-[assembly: System.Reflection.AssemblyProductAttribute("MapL")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MapL")]
+[assembly: System.Reflection.AssemblyProductAttribute("MapLTeste")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MapLTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
